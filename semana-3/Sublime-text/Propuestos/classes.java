4e0b3558955bdcaa40e050c94a37c064a68acfed
Alera9 <mcarrazanag@gmail.com>
2024-08-18 02:25:22 -0600

    Add: evidencia test 5

--- semana-3/Sublime-text/Propuestos/classes.java
+++ semana-3/Sublime-text/Propuestos/classes.java
@@ -260,3 +260,51 @@ class B extends A {
 	}
 }
 
+
+abstract class Animal {
+	private String name;
+	Animal(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return name;
+	}
+}
+class Dog extends Animal {
+	private String breed;
+	Dog(String breed) {
+		this.breed = breed;
+	}
+	Dog(String name, String breed) {
+		super(name);
+		this.breed = breed;
+	}
+	public String getBreed() {
+		return breed;
+	}
+}
+class Test {
+	public static void main(String[] args) {
+		Dog dog1 = new Dog("Beagle");
+		Dog dog2 = new Dog("Bubbly", "Poodle");
+		System.out.println(dog1.getName() + ":" + dog1.getBreed() +
+				":" +
+				dog2.getName() + ":" + dog2.getBreed());
+	}
+} 
+
+public class Main {
+	public static void main(String[] args) throws ParseException {
+		Queue<String> products = new ArrayDeque<String>();
+		products.add("p1");
+		products.add("p2");
+		products.add("p3");
+		System.out.println(products.peek());
+		System.out.println(products.poll());
+		System.out.println("");
+		products.forEach(s -> {
+			System.out.println(s);
+		});
+	}
+}
+
